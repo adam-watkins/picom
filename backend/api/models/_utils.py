b7f83d916d41cbda2aefffca6a12f42c9a5de9f4
quarterblue <quarterslate@gmail.com>
2021-09-14 12:55:11 -0700

    New fix output function

--- backend/api/models/_utils.py
+++ backend/api/models/_utils.py
@@ -1,7 +1,6 @@
 import pathlib
 from shutil import copytree
 
-from api import config
 from . import NestedPathMixin, IOPathMixin
 
 
@@ -11,16 +10,16 @@ def fix_output_dir(src_dir) -> str:
     there is a mismatch when copying the final jobs output to runs output
     To fix this issue, the path is deconstructed and the id is decremented once
     """
-
     src_dir = str(src_dir)
-    src_prefix = src_dir[:len(config.PIPELINE_OUTPUT_PRE_DIR)]
-    src_suffix = src_dir[-len(config.PIPELINE_OUTPUT_SUF_DIR):]
-    src_id = src_dir[:-len(config.PIPELINE_OUTPUT_SUF_DIR)
-                     ][len(config.PIPELINE_OUTPUT_PRE_DIR):]
-    src_id = int(src_id) - 1
-    src_dir = src_prefix + str(src_id) + src_suffix
+    id_pos = 0
+    dir_split = src_dir.split('/')
+    for idx, word in enumerate(dir_split):
+        if word.isdigit():
+            id_pos = idx
+    dir_split[id_pos] = str(int(dir_split[id_pos]) - 1)
+    new_dir = '/'.join(dir_split)
 
-    return src_dir
+    return new_dir
 
 
 def copy_model_fs(src: NestedPathMixin, dst: IOPathMixin, dst_subdir='input', src_subdir='output', final_node=False):
@@ -32,7 +31,6 @@ def copy_model_fs(src: NestedPathMixin, dst: IOPathMixin, dst_subdir='input', sr
         ValueError('src_subdir kwarg can only be "input" or "output"')
     if dst_subdir not in ['input', 'output']:
         ValueError('dst_subdir kwarg can only be "input" or "output"')
-
     src_dir = pathlib.Path(src.get_abs_path(subdir=src_subdir)).resolve()
     dst_dir = pathlib.Path(dst.get_abs_path(subdir=dst_subdir)).resolve()
 
